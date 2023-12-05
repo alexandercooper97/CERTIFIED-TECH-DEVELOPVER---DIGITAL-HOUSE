1d3c1db1c545be9f7a4e36169c888eb0042e87f4
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-05 16:41:34 -0500

    CLASE 04

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C04_VARIABLES_TIPOS_DATOS_OPERADORES/T2_VARIABLES_TIPOS_DATOS/Ejercicio_04.js
@@ -0,0 +1,11 @@
+/*
+Para este ejercicio, debemos declarar 4 variables de 
+tipo let con los nombres: verdadero, nada, texto y numero. 
+A cada una de ellas, tendremos que asignarle 
+el tipo de dato que sugiera su nombre, según corresponda.
+*/
+
+let verdadero = true;
+let nada = null;
+let texto = "University";
+let numero = 7;
