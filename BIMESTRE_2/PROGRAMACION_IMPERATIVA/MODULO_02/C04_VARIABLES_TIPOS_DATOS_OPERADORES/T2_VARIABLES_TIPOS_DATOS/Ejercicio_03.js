4235b65c8622a35d4f28cf5e9e523c715c0f96ea
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-05 16:34:33 -0500

    CLASE 04

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C04_VARIABLES_TIPOS_DATOS_OPERADORES/T2_VARIABLES_TIPOS_DATOS/Ejercicio_03.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 3: Constantes - P Imp
+
+Ahora, debemos crear una constante llamada EDAD_MINIMA y otra llamada EDAD_MAXIMA, 
+a las cuales les asignaremos valores numéricos 18 y 99 respectivamente.
+*/
+
+let EDAD_MINIMA = 18;
+let EDAD_MAXIMA = 99;
