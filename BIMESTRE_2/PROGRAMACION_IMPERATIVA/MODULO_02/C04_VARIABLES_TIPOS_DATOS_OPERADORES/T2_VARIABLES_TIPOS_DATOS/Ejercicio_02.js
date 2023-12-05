c3ff5902449fb82f639210b393ab07c5192fdbe2
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-05 16:32:19 -0500

    CLASE 04

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C04_VARIABLES_TIPOS_DATOS_OPERADORES/T2_VARIABLES_TIPOS_DATOS/Ejercicio_02.js
@@ -0,0 +1,8 @@
+/*
+Ejercicio 2: Asignando valores a las variables
+
+Declaremos dos variables llamadas edad y peso, a las cuales debemos asignarles un valor numérico.
+*/
+
+let edad = 25;
+let peso = 70;
