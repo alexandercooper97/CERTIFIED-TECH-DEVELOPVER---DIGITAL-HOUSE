45d49f6253f669ea26c1f6ac6f6dab5692dc10b3
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-05 19:56:32 -0500

    CLASE 05

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C05_FUNCIONES/T1_FUNCIONES_REGULARES/Ejercicio_01.js
@@ -0,0 +1,10 @@
+/*
+Nuestra tarea es crear una función que se va a llamar ejemplo 
+y tiene que devolver un String diciendo "hola, soy una función".
+*/
+
+function ejemplo(){
+    return "hola, soy una función";
+}
+
+ejemplo();
