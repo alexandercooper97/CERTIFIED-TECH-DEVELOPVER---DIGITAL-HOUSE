0b0b322fd5771316c60a2d3208055d487c3e12dc
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-05 19:45:24 -0500

    CLASE 04

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C04_VARIABLES_TIPOS_DATOS_OPERADORES/T3_OPERADORES/Ejercicio_01.js
@@ -0,0 +1,31 @@
+/*
+Declarar y asigna dos variables: primerNumero y segundoNumero.
+
+Luego, declarar las variables resultadoSuma, resultadoResta, 
+resultadoMultiplicacion, resultadoDivision y en cada una almacena 
+el resultado que corresponda.
+
+Por último, imprimir cada uno de los resultados utilizando 
+console.log(nombreDeLaVariable).
+
+let primerNumero = 100
+let segundoNumero = 5
+
+let resultadoSuma = primerNumero + segundoNumero // 105
+let resultadoResta = primerNumero - segundoNumero // 95
+let resultadoMultiplicacion = primerNumero * segundoNumero // 500
+let resultadoDivision = primerNumero / segundoNumero // 20
+*/
+
+let primerNumero = 100;
+let segundoNumero = 5;
+
+let resultadoSuma = primerNumero + segundoNumero; // 105
+let resultadoResta = primerNumero - segundoNumero; // 95
+let resultadoMultiplicacion = primerNumero * segundoNumero; // 500
+let resultadoDivision = primerNumero/segundoNumero; // 20
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion)
+console.log(resultadoDivision);
