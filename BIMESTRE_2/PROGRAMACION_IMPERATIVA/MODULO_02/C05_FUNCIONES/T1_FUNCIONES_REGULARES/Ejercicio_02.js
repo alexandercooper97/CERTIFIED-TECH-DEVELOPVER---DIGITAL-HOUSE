7447304863a542a586c62e057d01387b3ee228d0
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-05 20:09:55 -0500

    CLASE 05

--- BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C05_FUNCIONES/T1_FUNCIONES_REGULARES/Ejercicio_02.js
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C05_FUNCIONES/T1_FUNCIONES_REGULARES/Ejercicio_02.js
@@ -1,3 +1,9 @@
+/*
+Primero creamos una variable con el nombre  ejemplo y 
+le asignamos como valor una función. 
+La misma debe retornar un String que diga: 
+"hola, soy una función expresada".
+*/
 let ejemplo = function(){
     return "hola, soy una función expresada";
 }
