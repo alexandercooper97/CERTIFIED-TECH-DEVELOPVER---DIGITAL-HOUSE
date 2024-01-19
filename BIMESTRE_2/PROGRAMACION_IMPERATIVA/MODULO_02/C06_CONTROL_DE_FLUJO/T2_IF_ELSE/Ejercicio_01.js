cbab17d5163498cd176917866edc17970f3e0b78
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-18 19:29:06 -0500

    CLASE 06

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C06_CONTROL_DE_FLUJO/T2_IF_ELSE/Ejercicio_01.js
@@ -0,0 +1,20 @@
+/*
+Mi primer if
+
+Lograremos con este ejercicio realizar un simple if.
+
+Declaremos una variable con el nombre dato y 
+asignémosle un valor booleano.
+
+Luego, deberemos hacer un if que evalúe esa variable y, 
+si el valor es igual a  true, que imprima por consola "es true". 
+Caso contrario, que imprima por consola "es false".
+*/
+let dato = true;
+
+if (dato == true) {
+    console.log("es true");
+}
+else{
+    console.log("es false");
+}
