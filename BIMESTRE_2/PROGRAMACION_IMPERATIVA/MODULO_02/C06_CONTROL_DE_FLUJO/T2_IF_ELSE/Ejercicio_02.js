3bed1a6733e32d7cd7490561c33a527eac75f512
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-18 19:29:29 -0500

    CLASE 06

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C06_CONTROL_DE_FLUJO/T2_IF_ELSE/Ejercicio_02.js
@@ -0,0 +1,19 @@
+/*
+Igualdad, if y else
+¡Juguemos con condiciones!
+
+En este ejercicio nos viene dada una variable llamada lenguaje, 
+que tiene asignado un string. Nuestro trabajo es construir 
+un condicional if/else para que, si la variable contiene "javascript", 
+imprima por consola el texto "Estoy aprendiendo". 
+De lo contrario, que imprima"Lo aprenderé más adelante".
+*/
+
+let lenguaje = "javascript";
+
+if (lenguaje == "javascript"){
+    console.log("Estoy aprendiendo");
+}
+else{
+    console.log("Lo aprenderé más adelante");
+}
