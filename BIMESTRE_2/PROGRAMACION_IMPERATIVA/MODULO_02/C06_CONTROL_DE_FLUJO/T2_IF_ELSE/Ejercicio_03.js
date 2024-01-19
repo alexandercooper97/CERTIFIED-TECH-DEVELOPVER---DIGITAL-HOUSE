4c15768b25a522146a40833072aa48d6de0521a2
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-18 19:29:52 -0500

    CLASE 06

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C06_CONTROL_DE_FLUJO/T2_IF_ELSE/Ejercicio_03.js
@@ -0,0 +1,25 @@
+/*
+¿Puede pasar cosme fulanito?
+¡Vamos a completar la función!
+
+Debemos completar la función puedePasar para que, 
+analizando el nombre que recibe por parámetro, 
+determine si la persona puede pasar o no. 
+Si la persona se llama "Cosme Fulanito"", 
+no lo dejará pasar y lo indicará retornando false; 
+si la persona se llama de otra forma, lo dejará pasar 
+y lo indicará retornando true.
+
+function puedePasar(nombre) {
+    ???
+}
+*/
+
+function puedePasar(nombre) {
+    if (nombre == "Cosme Fulanito"){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
