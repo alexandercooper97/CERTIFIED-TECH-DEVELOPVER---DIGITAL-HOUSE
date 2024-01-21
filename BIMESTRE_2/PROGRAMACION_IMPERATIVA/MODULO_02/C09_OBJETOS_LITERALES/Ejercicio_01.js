c279549457a2e3607282fcb1c21614f21628ccb0
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-21 12:50:47 -0500

    CLASE 09

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C09_OBJETOS_LITERALES/Ejercicio_01.js
@@ -0,0 +1,23 @@
+/*
+Ejercicio1: Creando un objeto literal
+Vamos a ver cómo crear nuestro objeto "perro":
+
+Declarar una variable llamada perro de tipo objeto literal.
+Esa variable debe tener 3 propiedades:
+
+1. nombre, con un valor de tipo String.
+
+2. edad, con un valor de tipo Number.
+
+3. vacunado, con un valor de tipo Boolean.
+*/ 
+
+let perro = {
+    nombre : "Toby",
+    edad : 3,
+    vacunado : true
+}
+
+console.log(perro.nombre);
+console.log(perro.edad);
+console.log(perro.vacunado);
