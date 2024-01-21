e81597e75d117266edd11361186b7cd160554d9a
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-20 20:27:08 -0500

    CLASE 08

---
+++ BIMESTRE_2/PROGRAMACION_IMPERATIVA/MODULO_02/C08_STRINGS_ARRAYS/T3_METODOS_STRING/Ejercicio_02.js
@@ -0,0 +1,14 @@
+/*
+Ejercicio 2: JS_funciones_contar_caracteres
+Lo primero que necesitamos es declarar una variable llamada texto, 
+asignándole como valor un texto (string), el que queramos. 
+Luego, utilizando la propiedad length, tendremos que imprimir 
+en consola el total de caracteres que contiene la frase.
+
+*/
+
+let texto = "Alexander";
+
+console.log(texto.length);
+
+
