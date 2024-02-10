91b12e9a9a1c7a9250b9d58edfad22fd275a0e8b
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-02-10 05:25:48 -0500

    CLASE 02

---
+++ BIMESTRE_3/PROGRAMACION_ORIENTADA_OBJETOS/MODULO_01/C02_INTRO_JAVA/T1_STRING_INTEGER_FLOAT/Ejercicio1.java
@@ -0,0 +1,15 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // Definir dos cadenas de texto
+        String cadena1 = "Hola";
+        String cadena2 = "Hola";
+
+        // Comprobar si las cadenas son iguales
+        if (cadena1.equals(cadena2)) {
+            System.out.println("Las cadenas son iguales.");
+        } else {
+            System.out.println("Las cadenas son distintas.");
+        }
+    }
+}
+
