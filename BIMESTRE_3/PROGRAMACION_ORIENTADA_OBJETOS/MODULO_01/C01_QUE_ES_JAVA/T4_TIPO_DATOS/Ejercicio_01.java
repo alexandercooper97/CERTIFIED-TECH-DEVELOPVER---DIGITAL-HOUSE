7c0e2acd75933bb0242f379d3756fa99eca1e274
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-02-10 04:06:11 -0500

    CLASE 01

---
+++ BIMESTRE_3/PROGRAMACION_ORIENTADA_OBJETOS/MODULO_01/C01_QUE_ES_JAVA/T4_TIPO_DATOS/Ejercicio_01.java
@@ -0,0 +1,16 @@
+/*
+ * Es hora de que crees tus primeras variables en java
+
+La idea es que crees tres variables: una que se va a llamar 
+numeroEntero y va a ser de tipo int, una que se va a llamar 
+numeroConComa de tipo double y por último una llamada nombre 
+de tipo string
+
+Luego de declararlas asignarles un valor acorde a su tipo
+ */
+
+public class Ejercicio_01 {
+    int numeroEntero = 27;
+    double numeroConComa = 15.1;
+    String nombre = "Miguel";
+}
