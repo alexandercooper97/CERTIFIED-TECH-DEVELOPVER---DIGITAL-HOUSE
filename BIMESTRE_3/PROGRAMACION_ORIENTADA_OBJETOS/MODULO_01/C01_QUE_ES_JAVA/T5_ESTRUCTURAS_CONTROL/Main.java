df0c9d49c9a14b7c244204a30644ccda048c6932
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-02-10 04:12:32 -0500

    CLASE 01

---
+++ BIMESTRE_3/PROGRAMACION_ORIENTADA_OBJETOS/MODULO_01/C01_QUE_ES_JAVA/T5_ESTRUCTURAS_CONTROL/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Definir y asignar valores a los números enteros
+        int numero1 = 20;
+        int numero2 = 5;
+
+        // Comprobar si numero1 es divisible por numero2
+        if (numero1 % numero2 == 0) {
+            System.out.println(numero1 + " es divisible por " + numero2);
+        } else {
+            System.out.println(numero1 + " no es divisible por " + numero2);
+        }
+    }
+}
