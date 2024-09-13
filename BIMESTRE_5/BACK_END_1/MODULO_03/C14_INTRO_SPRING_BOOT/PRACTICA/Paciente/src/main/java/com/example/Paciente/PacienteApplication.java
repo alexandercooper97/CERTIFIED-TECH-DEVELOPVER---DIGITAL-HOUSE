203c7553141c437fb9da0963a37426913c8310e6
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-09-13 09:03:11 -0500

    CLASE 14

---
+++ BIMESTRE_5/BACK_END_1/MODULO_03/C14_INTRO_SPRING_BOOT/PRACTICA/Paciente/src/main/java/com/example/Paciente/PacienteApplication.java
@@ -0,0 +1,25 @@
+package com.example.Paciente;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+
+public class PacienteApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PacienteApplication.class, args);
+
+	}
+	@GetMapping
+	public String holaPAciente(){
+		return "Hola Paciente";
+	}
+
+}
